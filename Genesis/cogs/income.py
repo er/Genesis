bb7b7325064d1ef2ac056abe29d065f608900a2f
Jordan <jordansdns@gmail.com>
2021-10-10 12:28:48 +0100

    Added daily, weekly, monthly

--- Genesis/cogs/income.py
+++ Genesis/cogs/income.py
@@ -6,31 +6,79 @@ from discord.ext import commands
 
 from Genesis.constants import JOBS
 from Genesis.utils import generate_embed
+from Genesis.utils import is_claimable
 
 
 class Income(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.seconds_in_a_day = 86400
+        self._cd = commands.CooldownMapping.from_cooldown(
+            1, 900, commands.BucketType.user
+        )
+        self.bounds = {
+            "daily": {
+                "lower": 200,
+                "upper": 300,
+            },
+            "weekly": {"lower": 1000, "upper": 1200},
+            "monthly": {"lower": 3500, "upper": 3800},
+        }
 
     async def generate_job(self, user_id: int) -> discord.Embed:
         job = random.choice(JOBS)
-        amount = random.randint(25, 60)
-        multiplier = await self.bot.pool.fetchval("SELECT multiplier FROM users WHERE discord_id=$1", user_id)
-        return await generate_embed(job.replace("%amt%", f"${amount * multiplier}"))
+        amount = random.randint(5, 20)
+        await self.bot.pool.execute(
+            f"UPDATE users SET balance=balance + $1 WHERE discord_id=$2",
+            amount,
+            user_id,
+        )
+        return await generate_embed(job.replace("%amt%", f"${amount}"))
+
+    async def get_cooldown(self, ctx) -> int:
+        """Returns the cooldown left"""
+        bucket = self._cd.get_bucket(ctx.message)
+        return bucket.update_rate_limit()
 
     @commands.command(aliases=["w"])
     async def work(self, ctx) -> discord.Embed:
+        if (cooldown := await self.get_cooldown(ctx)) is not None:
+            if isinstance(cooldown, float) or isinstance(cooldown, int):
+                return await ctx.send(
+                    embed=await generate_embed(
+                        f"You can work again <t:{int(time.time() + cooldown)}:R>"
+                    )
+                )
         return await ctx.reply(embed=await self.generate_job(ctx.author.id))
 
-    @commands.command()
-    async def daily(self, ctx) -> discord.Embed:
-        last_claimed = await self.bot.pool.fetchval("SELECT daily_claimed_at FROM users WHERE discord_id=$1", ctx.author.id)
-        if last_claimed + self.seconds_in_a_day > time.time():
-            return await ctx.send(embed=await generate_embed(f"You can claim your daily rewards in "
-                                                             f"<t:{last_claimed + self.seconds_in_a_day}:R>!"))
-
-
+    @commands.command(aliases=["daily", "weekly", "monthly"])
+    async def _interval_rewards(self, ctx) -> discord.Embed:
+        interval = ctx.invoked_with.lower()
+        last_claimed = await self.bot.pool.fetchval(
+            f"SELECT {interval}_claimed_at FROM users WHERE discord_id=$1",
+            ctx.author.id,
+        )
+        if (time_until := await is_claimable(interval, last_claimed)) > 0:
+            return await ctx.send(
+                embed=await generate_embed(
+                    f"You can claim your {interval} rewards "
+                    f"<t:{int(time.time()) + time_until}:R>!"
+                )
+            )
+        amount = random.randint(
+            self.bounds[interval]["lower"], self.bounds[interval]["upper"]
+        )
+        await self.bot.pool.execute(
+            f"UPDATE users SET balance=balance + $1, {interval}_claimed_at=$2 "
+            f"WHERE discord_id=$3",
+            amount,
+            int(time.time()),
+            ctx.author.id,
+        )
+        return await ctx.send(
+            embed=await generate_embed(
+                f"You claimed your daily rewards and got ${amount}!"
+            )
+        )
 
 
 def setup(bot):
--- Genesis/utils.py
+++ Genesis/utils.py
@@ -1,5 +1,13 @@
+import time
+
 import discord
 
 
 async def generate_embed(description: str) -> discord.Embed:
     return discord.Embed(description=description, colour=0x000000)
+
+
+async def is_claimable(interval: str, last_claimed: int) -> int:
+    delay = {"daily": 86400, "weekly": 604800, "monthly": 2419200}
+    print((last_claimed + delay[interval]) - int(time.time()))
+    return (last_claimed + delay[interval]) - int(time.time())
